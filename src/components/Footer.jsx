b834d51e6de3c235d51934ec16b7124b4d4b6d12
Pavan Kamthane <95903010+Pavan-Kamthane@users.noreply.github.com>
2024-04-09 17:56:03 +0530

    Project setup done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+// import Button from 'react-bootstrap/Button';
+import "../styles/footer.css";
+
+const Footer = () => {
+  return (
+    <>
+     
+
+    <div className="footer-copyright text-center py-3">© 2024 Copyright:
+        <a href="/"> 
+            PG_Pedia
+        </a>
+    </div>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/pages/Home.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from 'react-bootstrap/Button';
+import "../styles/Home.css";
+import home_img from "../assets/home img.jpeg";
+
+const Home = () => {
+  return (
+    <>
+    <Container className="home">
+
+      <Row>
+        <Col className="col1">
+            <h1>PG-<span>Pedia</span></h1>
+            <p>
+                {/* A Portal that manages the PG dissertation */}
+                {/* make 15 word para on above */}
+                A Portal that manages the PG dissertation of students and teachers. It provides a platform for students to submit their thesis and for teachers to evaluate them.
+                
+            </p>
+            <Button>
+                Get Started
+            </Button>
+        </Col>
+        <Col>
+            <img
+                src={home_img}
+                alt="home"
+            />
+        </Col>
+      </Row>
+    </Container>
+
+    <Container className="about">
+        <Row className="row1">
+            <Col>
+                <h1>About Us</h1>
+            </Col>
+        </Row>
+        <Row className="row2">
+            <Row>
+                <Col>
+                    <h2>
+                        What is use of PG Pedia?
+                    </h2>
+                    <p>
+                        PG Pedia is a portal that manages the PG dissertation of students and teachers. It provides a platform for students to submit their thesis and for teachers to evaluate them.
+                    </p>
+                </Col>
+                <Col>
+                    <h2>
+                        How to use PG Pedia?
+                    </h2>
+                    <p>
+                        Students can submit their thesis and teachers can evaluate them. Students can also view their grades and feedback given by teachers.
+                    </p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <h2>
+                        What is PG Dissertation?
+                    </h2>
+                    <p>
+                        A dissertation is a long piece of writing on a particular subject, especially one that is done in order to receive a degree at college or university.
+                        A dissertation is a piece of academic writing normally submitted as part of a postgraduate degree, whether it's your master's or PhD.
+                    </p>
+                </Col>
+            </Row>
+        </Row>
+    </Container>
+
+    {/* contact us form */}
+    <Container className="contact">
+        <Row className="row1">
+            <Col>
+                <h1>Contact Us</h1>
+            </Col>
+        </Row>
+        <Row className="row2">
+            <Col>
+                <form>
+                    <input type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="text" placeholder="Subject" />
+                    <textarea placeholder="Message"></textarea>
+                    <Button>Send</Button>
+                </form>
+            </Col>
+        </Row>
+    </Container>
+    </>
+  );
+};
+
+export default Home;
